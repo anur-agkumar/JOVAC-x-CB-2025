043806f167fced24398ee84faf5dc3a75ce8cd3c
hansrajathole <hansrajathole1@gmail.com>
2025-07-20 16:23:00 +0530

    ejs how to show data and middleware

--- LEC 5 EJS/src/routes/user.routes.js
+++ LEC 5 EJS/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const userModel = require("../model/user.model")
+const mongoose = require("mongoose")
 const router = express.Router()
 // /register
 
@@ -39,6 +40,63 @@ router.get("/register-successful", (req,res)=>{
     res.render("registered", {title : "successful"})
 })
 
+router.patch("/update/:id",async (req, res)=>{
+    let userId = req.params.id
+    let { name } = req.query
+    console.log(userId);
+    
+    if(!userId){
+        return res.json({message : "user id is required"})
+    }
+
+    // false ----> ! ----> true
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        return res.json({message : "Invalid user id"})
+    }
+
+    
+
+    if(!name){
+        return res.json({message : "name is required"})
+    }
+
+    let user = await userModel.findById(userId)
+    // null ----> falsy value
+
+    if(!user){
+        return res.json({message : "user not found"})
+    }
+
+
+    await userModel.findByIdAndUpdate(
+        {_id : userId} ,
+        {name : name}
+)
+    console.log(user);
+    
+    res.json({message : "user update successful" })
+    
+})
+
+
+router.delete("/delete/:id", async (req, res)=>{
+    let userId = req.params.id
+
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        return res.json({message : "Invalid user id"})
+    }
+
+    let user = await userModel.findById(userId)
+    // null ----> falsy value
+
+    if(!user){
+        return res.json({message : "user not found"})
+    }
+
+    const userData = await userModel.findByIdAndDelete(userId)
+
+    res.json({message : "user data deleted successfully...", userData})
+})
 
 
 
