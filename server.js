a6efee71886a05de3124a05192fb1c2886ff3932
anur-agkumar <145280405+anur-agkumar@users.noreply.github.com>
2025-08-01 10:39:03 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app")
-const connect = require("./src/db/db")
-const PORT = 3000
-
-
-app.listen(PORT , ()=>{
-    console.log("server is runnig on port : ", PORT);
-    connect()
-})
